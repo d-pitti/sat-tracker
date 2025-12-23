404785fc72ef40ed6890689fed85b4633d052e26
d-pitti <dpittimotive@gmail.com>
2025-12-23 12:20:34 -0500

    updated security variables

--- src/app/lib/api/route.tsx
+++ src/app/lib/api/route.tsx
@@ -1,8 +1,26 @@
 import { NextResponse } from "next/server";
 
+const params = new URLSearchParams({ identity: process.env.URL_USER!, password: process.env.URL_PASS!});
+
+
+export async function getCredentials() { 
+    const cookieStore = await fetch( 'https://www.space-track.org/ajaxauth/login', {
+        method: 'POST', 
+        body:params,
+    })
+
+    return cookieStore;
+}
 
 export async function GET(){
-    const results = await fetch('https://www.space-track.org/basicspacedata/query/class/gp/orderby/TLE_LINE0%20asc/limit/1000/emptyresult/show');
+    const cookieSession = await getCredentials();    
+    console.log(cookieSession);
+
+    const results = await fetch('https://www.space-track.org/basicspacedata/query/class/gp/orderby/TLE_LINE0%20asc/limit/1000/emptyresult/show', { 
+       headers: {
+        'Cookie': cookieSession.headers.getSetCookie()[0], 
+       }
+    });
     const data =await results.json();
 
     return NextResponse.json({data});
