1be99fdf13609f06259002c0ff634b5a7f787007
d-pitti <dpittimotive@gmail.com>
2025-12-27 20:58:05 -0500

    file organization

--- src/app/lib/seed/seed.ts
+++ src/app/lib/seed/seed.ts
@@ -1,28 +1,16 @@
-import { create } from 'domain';
 import { getJson } from '../api/getData';
-import mysql from 'mysql2/promise';
 import pool from '../db/db';
 
 
 // establish connection to database and get satellite data
 export async function dbInitalize() {
+    
     try {
-
-        //First create mysql connection and establish schema 
-        const connection = await mysql.createConnection({
-                    host: process.env.DB_HOST!,
-                    port: Number.parseInt(process.env.DB_PORT!),
-                    user: process.env.DB_USER!,
-                    password: process.env.DB_PASS!,
-                    
-        });
-
-        const createSchema = 'CREATE DATABASE IF NOT EXISTS space_tracker CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;';
-        await connection.execute(createSchema);
-        connection.end();
+        const poolConnection = await pool.getConnection(); 
+        const createSchema = 'CREATE DATABASE IF NOT EXISTS space_tracker;';
+        await poolConnection.execute(createSchema);
         
         // then establish pool connection to create table and seed the table
-        const poolConnection = await pool.getConnection(); 
         const createTable = 'CREATE TABLE IF NOT EXISTS satellites ( OBJECT_NAME VARCHAR(255) PRIMARY KEY, TLE_LINE_ONE VARCHAR(255), TLE_LINE_TWO VARCHAR(255) );';
         await poolConnection.execute(createTable);
 
