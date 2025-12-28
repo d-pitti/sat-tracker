fbae96e6fd4088c9232ec620b2c88db0fa12980e
d-pitti <dpittimotive@gmail.com>
2025-12-27 19:09:00 -0500

    fix db establish schema and conneciton. setup docker.

--- src/app/lib/seed/seed.ts
+++ src/app/lib/seed/seed.ts
@@ -1,4 +1,6 @@
+import { create } from 'domain';
 import { getJson } from '../api/getData';
+import mysql from 'mysql2/promise';
 import pool from '../db/db';
 
 
@@ -6,20 +8,33 @@ import pool from '../db/db';
 export async function dbInitalize() {
     try {
 
-        const connection = await pool.getConnection();
-        
-        const createTable = 'CREATE TABLE IF NOT EXISTS satellites ( OBJECT_NAME VARCHAR(255) PRIMARY KEY, TLE_LINE_ONE VARCHAR(255), TLE_LINE_TWO VARCHAR(255) );';
+        //First create mysql connection and establish schema 
+        const connection = await mysql.createConnection({
+                    host: process.env.DB_HOST!,
+                    port: Number.parseInt(process.env.DB_PORT!),
+                    user: process.env.DB_USER!,
+                    password: process.env.DB_PASS!,
+                    
+        });
 
-        await connection.execute(createTable);
+        const createSchema = 'CREATE DATABASE IF NOT EXISTS space_tracker CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;';
+        await connection.execute(createSchema);
+        connection.end();
+        
+        // then establish pool connection to create table and seed the table
+        const poolConnection = await pool.getConnection(); 
+        const createTable = 'CREATE TABLE IF NOT EXISTS satellites ( OBJECT_NAME VARCHAR(255) PRIMARY KEY, TLE_LINE_ONE VARCHAR(255), TLE_LINE_TWO VARCHAR(255) );';
+        await poolConnection.execute(createTable);
 
         //get api data, map to sql values then execute a query to insert data to the database, store data results in array
         const values = await getJson();
         const mappedValues = values.map(item => [item.OBJECT_NAME, item.TLE_LINE1, item.TLE_LINE2]);
 
         const sql = "INSERT INTO satellites (OBJECT_NAME, TLE_LINE_ONE, TLE_LINE_TWO) VALUES ? ON DUPLICATE KEY UPDATE OBJECT_NAME=VALUES(OBJECT_NAME)";
-        await connection.query(sql, [mappedValues]);
+        await poolConnection.query(sql, [mappedValues]);
 
-        pool.releaseConnection(connection);
+        //release connection when done
+        pool.releaseConnection(poolConnection);
 
         console.log('DB Connected and Table created');
         // Return the query results as a JSON response
